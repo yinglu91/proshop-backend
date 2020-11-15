c61abea7f22e86809be952317dd21f69511f6b2b
ylu <ylu@ert.com>
2020-11-15 10:32:19 -0500

    get products from mongoDB

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js' 
+// .js is required for importing file !! reactjs doesn't need to
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running...???')
+})
+
+app.use('/api/products', productRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold )
+  }
+)
+
